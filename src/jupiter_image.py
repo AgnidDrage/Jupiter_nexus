97d7d88f7c17cf3fd48dc3e9fb8e39d7b25f5286
AgnidDrage <marianosancheztoledo@gmail.com>
2022-10-01 14:21:02 -0300

    Image channel merged

--- src/jupiter_image.py
+++ src/jupiter_image.py
@@ -1,9 +1,22 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("./images/JNCE_2022272_45C00001_V01-raw.png")
+blue = cv2.imread("./images/JNCE_2022272_45C00001_V01-blue.png", cv2.IMREAD_GRAYSCALE)
+green = cv2.imread("./images/JNCE_2022272_45C00001_V01-green.png", cv2.IMREAD_GRAYSCALE)
+red = cv2.imread("./images/JNCE_2022272_45C00001_V01-red.png", cv2.IMREAD_GRAYSCALE)
 
-print(image)
+#print shapes
+print("blue shape: ", blue.shape)
+print("green shape: ", green.shape)
+print("red shape: ", red.shape)
 
-plt.imshow(image)
+#merge 3 channels
+img = cv2.merge((red, green, blue))
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#plot
+plt.imshow(img)
 plt.show()
+
+
